63239b916deebc4bc146a1fe8021f0458b5c9963
Ulisses Soares <ulissessm@gmail.com>
2022-02-08 13:08:13 +0000

    connected to database

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 //const path = require('path')
 
-//const db = require('./database') // NÃO PRECISA INFORMAR O NOME DO ARQUIVO INDEX
+const db = require('./database/db') // SE O NOME DO ARQUIVO NÃO FOR INDEX É NECESSÁRIO INFORMAR O NOME DO ARQUIVO JS
 const routes = require('./routes')
 
 const app = express()
 
 // CONEXÃO COM O BANCO DE DADOS
-//db.connect()
+db.connect()
 
 // habilita server para receber dados via post (formulário)
 app.use(express.urlencoded({ extended: true}))
@@ -18,3 +18,5 @@ app.use('/api', routes)
 // executando o servidor
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
+
+
